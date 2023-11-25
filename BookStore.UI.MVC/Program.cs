42dfa332b44953586ae2ac986f3058bce5e129be
Harrison Smith <harrisonws320@gmail.com>
2023-11-24 19:03:43 -0600

    following directions of 2 tier model

--- BookStore.UI.MVC/Program.cs
+++ BookStore.UI.MVC/Program.cs
@@ -12,8 +12,11 @@ namespace BookStore.UI.MVC
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+
+          
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddRoleManager<RoleManager<IdentityRole>>().AddEntityFrameworkStores<ApplicationDbContext>();
